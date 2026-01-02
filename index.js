32142e8259bebe6e92a18b15a3973639f8a6918b
infancetony-69 <fullstacktony.69@gmail.com>
2026-01-02 04:20:19 +0000

    Update script for daily commits

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
 import jsonfile from "jsonfile";
 import moment from "moment";
 import simpleGit from "simple-git";
-import random from "random";
 
 const path = "./data.json";
 
-const markCommit = (x, y) => {
-  const date = moment()
-    .subtract(1, "y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
+// Configure Git user
+const git = simpleGit();
+await git.addConfig('user.name', 'infancetony-69');
+await git.addConfig('user.email', 'fullstacktony.69@gmail.com');
+
+const makeCommits = (n) => {
+  if (n === 218) {
+    console.log('All 218 commits created successfully!');
+    return git.push();
+  }
+  
+  // Start date: May 30, 2025 + n days
+  const date = moment('2025-05-30')
+    .add(n, 'd')
     .format();
 
   const data = {
     date: date,
   };
-
+  
+  console.log(`Creating commit ${n + 1}/218: ${date}`);
+  
   jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date }).push();
+    git.add([path])
+      .commit(date, { '--date': date }, makeCommits.bind(this, n + 1));
   });
 };
 
-const makeCommits = (n) => {
-  if(n===0) return simpleGit().push();
-  const x = random.int(0, 54);
-  const y = random.int(0, 6);
-  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
-
-  const data = {
-    date: date,
-  };
-  console.log(date);
-  jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
-  });
-};
-
-makeCommits(100);
+// Start from day 0 (May 30, 2025) to day 217 (Jan 2, 2026)
+// Total: 218 days
+makeCommits(0);
